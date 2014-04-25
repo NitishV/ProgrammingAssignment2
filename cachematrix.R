c9e7775bdc9a1f8b773bbdb25795c747756bd6c3
nitish <nitish.vashishtha@guavus.com>
2014-04-25 16:57:00 +0530

    Adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -6,14 +6,20 @@
 
 # Creates a cacheable matrix 'x' and its inverse, the function provides additional getters and setters for these objects  
 makeCacheMatrix <- function(x = matrix()) {
+  #Warn for empty matrix, empty matrix is considered invertible, but it not useful 
 	if(is.na(x)[1,1]) {
 		warning("Input matrix is empty!")	
 	}
-	if(nrow(x) != ncol(x) ){
+  #Check If the matrix has been incorrectly defined wrt to columns and rows
+	#A square matrix is a necessary condition for invertability, exit if not
+  if(nrow(x) != ncol(x) ){
 		stop("Expected a square matrix for inversion!")
 	}
+  
 	i <- NULL
-	set <- function(mat) {
+	
+  #Define custom functions. getters and setters for matrix and its inverse
+  set <- function(mat) {
 		x <<- mat
 		i <<- NULL
 	}
@@ -28,12 +34,17 @@ makeCacheMatrix <- function(x = matrix()) {
 # Calculates the inverse of matrix 'x' and returns it (after caching it), if the inverse has been cached already it is returned from cache instead. 
 cacheSolve <- function(x, ...) {
 	inv <- x$getInverse()
+  
+  #Check cache
 	if(!is.null(inv)) {
 		message("Found matrix inverse in cache. Loading result from cache.")
 		return(inv)
 	}
 	data <- x$get()
-	inv <- solve(data)
+  
+	#Solve for inverse
+  inv <- solve(data)
 	x$setInverse(inv)
-	inv
+	
+  inv
 }
